3607e676ed66cd6bf5256d464679b721bf954935
Farman Okz <farooblog@gmail.com>
2026-05-01 23:48:52 +0500

    we are live on version 1.0.1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from CI/CD on AWS!', version: '1.0.0' });
+  res.json({ message: 'Hello from CI/CD on AWS!', version: '1.0.1' });
 });
 
 app.get('/health', (req, res) => {
